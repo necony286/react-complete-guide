e9db361110d7e2d647baa698ac7019660501ab25
necony286 <necony286@gmail.com>
2020-10-16 19:00:48 +0200

    2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 function App() {
   return (
     <div className="App">
       <h1>Hi, I'm a React app!</h1>
+      <p>This is really working!</p>
+      <Person />
     </div>
   );
   // return React.createElement(
