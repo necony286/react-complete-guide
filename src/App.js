2af0ece48492b56f0096a1033b3ff5904cb212cc
necony286 <necony286@gmail.com>
2020-10-17 12:30:08 +0200

    37

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ function App() {
       <h1>Hi, I'm a React app!</h1>
       <p>This is really working!</p>
       <Person name="Max" age="28" />
-      <Person name="Manu" age="23" />
+      <Person name="Manu" age="23">
+        My hobbies: racing
+      </Person>
       <Person name="Step" age="25" />
     </div>
   );
