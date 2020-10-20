04e5d33e86ee7037f8d32ed87b3e206efabd4cf7
necony286 <necony286@gmail.com>
2020-10-20 15:23:19 +0200

    49

--- src/App.js
+++ src/App.js
@@ -39,11 +39,20 @@ const App = (props) => {
     });
   };
 
+  const style = {
+    backgroundColor: 'white',
+    font: 'inherit',
+    border: '1px solid blue',
+    padding: '8px',
+    cursor: 'pointer',
+  };
+
   return (
     <div className="App">
       <h1>Hi, I'm a React app!</h1>
       <p>This is really working!</p>
       <button
+        style={style}
         //bind syntax better if possible (performance hit)
         onClick={() => switchNameHandler('Maxxiimuss!')}
       >
